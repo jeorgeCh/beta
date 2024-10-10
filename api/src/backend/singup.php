127ad32732e9a8ca5611210618fd6a99a8e27b9f
jorge <jorgechicaiza2512@gmail.com>
2024-10-10 11:57:40 -0500

    Update

--- api/src/backend/singup.php
+++ api/src/backend/singup.php
@@ -7,11 +7,25 @@ require('../../config/db_connection.php');
 $email = $_POST['email'];
 $pass =  $_POST['passwd'];
 $enc_pass = md5($pass);
-$query = "INSERT INTO users (email,password) VALUES ('$email', '$enc_pass')";
+
+//validate if email already exist
+$query ="SELECT * from users where email='$email'";
 
 $result = pg_query($conn, $query);
+$row =pg_fetch_assoc($result);
+if ($row) {
+    echo"<script>alert('Email already exist')</script>";
+    header('refresh:0; url=http://127.0.0.1/beta//api/src/register_form.html'); 
+    exit();	
+} 
+$query = "INSERT INTO users (email,password)
+ VALUES ('$email', '$enc_pass')";
+
+$result= pg_query($conn, $query);
+
 if ($result) {
-    echo"Registration succesful!";
+    echo"<script>alert('Registration successful!')</script>";
+    header('refresh:0; url=http://127.0.0.1/beta//api/src/login_form.html');     
 } else {echo"Registration failed!";}
 
 pg_close($conn);
